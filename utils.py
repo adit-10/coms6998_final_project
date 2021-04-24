50f511a716586fbb11560cf05a6ea30934c8e61f
adit-10 <80546292+adit-10@users.noreply.github.com>
2021-04-23 23:27:02 -0400

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,74 @@
+# Utility functions for applying optimizations
+
+import tensorflow as tf
+
+
+def dynamic_range_quantize(model):
+  #Apply dynamic quantization to model
+  converter = tf.lite.TFLiteConverter.from_keras_model(model)
+  converter.optimizations = [tf.lite.Optimize.DEFAULT]
+  tflite_model_dynamic_quant = converter.convert()
+  return tflite_model_dynamic_quant
+
+def full_integer_quantize(representative_dataset, model):
+  #Apply full integer quantization to model
+  converter = tf.lite.TFLiteConverter.from_keras_model(model)
+  converter.optimizations = [tf.lite.Optimize.DEFAULT]
+  converter.representative_dataset = representative_data_gen
+
+  tflite_model_full_integer_quant = converter.convert()
+  return tflite_model_full_integer_quant
+
+def float_quantize(model):
+  ##Apply float quantization to model
+  converter = tf.lite.TFLiteConverter.from_keras_model(model)
+  converter.optimizations = [tf.lite.Optimize.DEFAULT]
+  converter.target_spec.supported_types = [tf.float16]
+  tflite_model_float16_quant = converter.convert()
+  return tflite_model_float16_quant
+
+
+def get_gzipped_model_size(file):
+    # Returns size of gzipped model, in bytes.
+    import os
+    import zipfile
+
+    _, zipped_file = tempfile.mkstemp('.zip')
+    with zipfile.ZipFile(zipped_file, 'w', compression=zipfile.ZIP_DEFLATED) as f:
+        f.write(file)
+
+    return os.path.getsize(zipped_file)
+
+def evaluate_model(x_test, y_test, tflite_model):
+    #Evaluate tflite model
+
+    interpreter = tf.lite.Interpreter(model_content=tflite_model)
+    interpreter.allocate_tensors()
+
+    prediction_digits = []
+    input_index = interpreter.get_input_details()[0]["index"]
+    output_index = interpreter.get_output_details()[0]["index"]
+
+  # Run predictions on ever y image in the "test" dataset.
+    for i, test_image in enumerate(x_test, y_test):
+        if i % 1000 == 0:
+            print('Evaluated on {n} results so far.'.format(n=i))
+        # Pre-processing: add batch dimension and convert to float32 to match with
+        # the model's input data format.
+        test_image = np.expand_dims(test_image, axis=0).astype(np.float32)
+        interpreter.set_tensor(input_index, test_image)
+
+        # Run inference.
+        interpreter.invoke()
+
+        # Post-processing: remove batch dimension and find the digit with highest
+        # probability.
+        output = interpreter.tensor(output_index)
+        digit = np.argmax(output()[0])
+        prediction_digits.append(digit)
+
+    print('\n')
+    # Compare prediction results with ground truth labels to calculate accuracy.
+    prediction_digits = np.array(prediction_digits)
+    accuracy = (prediction_digits == y_test).mean()
+    return accuracy
